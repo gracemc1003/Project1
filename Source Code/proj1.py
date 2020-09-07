0c3b4810ec78c37a3ff75049e07f1d5f6e7faad3
gracemc1003 <33295345+gracemc1003@users.noreply.github.com>
2020-09-07 14:41:49 -0500

    Add files via upload

---
+++ Source Code/proj1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  7 11:56:52 2020
+
+@author: gracemcmonagle
+"""
+
+import pandas as pd
+import math
+import numpy
+
+stressData = pd.read_csv('https://query.data.world/s/4p3ajeh3gdrbpcyvsn4m7scstwsp4w')
+goodEdData = pd.read_csv('https://query.data.world/s/cebxvbdi2ma2aviha2muijmdsbp7kv')
+socData = pd.read_csv('https://query.data.world/s/u4reutgg76ew5lt5dzwjdhuynrcllv')
+
